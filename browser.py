60ffa5bd5a93e3908de460afe135fe6cb8c3e01c
Your Name <you@example.com>
2025-10-02 10:21:15 +0300

    upd

---
+++ browser.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import sync_playwright
+from loguru import logger
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+LOGIN = os.getenv('LOGIN')
+PASSWORD = os.getenv('PASSWORD')
+logger.info("Запуск браузера...")
+
+with sync_playwright() as p:
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days", backtrace=True, diagnose=True)
+
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://journal.top-academy.ru/ru/auth/login/index")
+    logger.info("Страница загружена")
+
+    page.wait_for_selector('input[name="username"]', timeout=10000)
+
+    page.fill('input[name="username"]', LOGIN)
+    page.fill('input[name="password"]', PASSWORD)
+    logger.info("Данные для входа введены")
+
+    page.click('button[type="submit"]')
+    logger.info("Кнопка входа нажата")
+
+    page.wait_for_timeout(5000)
+
+    logger.success(f"Вход выполнен! Текущий URL: {page.url}")
+
+    page.goto("https://journal.top-academy.ru/ru/main/homework/page/index")
+
+    logger.info("Находимся в разделе домашнего задания")
+
+    input("Вход выполнен. Нажмите Enter для закрытия...")
+
+    browser.close()
